b9db58f613a0b8272456bd3065a0acb298648c38
MariandreaMV <mariandrea.mv15@gmail.com>
2018-03-02 23:43:19 -0430

    small changes in views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>System</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                       
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
@@ -79,16 +79,20 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SPORTS TOURNAMENTS
                 </div>
-
+                @auth
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('new-team') }}">Create new team </a>
+                    <a href="">Register team to a tournament</a>
+                    <?php if (\Auth::user()->status==1): ?>
+                    <a href="">Register a new tournament</a>
+                    <a href="">Teams in tournaments</a>
+                    <a href="">Tournaments</a>
+                    <a href="{{ url('/admin') }}">Admin</a>
+                    <?php endif ?>
                 </div>
+                @endauth
             </div>
         </div>
     </body>
